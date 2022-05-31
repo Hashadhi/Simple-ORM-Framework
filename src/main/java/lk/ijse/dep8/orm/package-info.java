13441211a6e0f8d36c0ae114558b43f4e6952438
Hashadhi <hashadhijayasinghe@gmail.com>
2022-05-31 08:39:46 +0530

    Add package doc comment

---
+++ src/main/java/lk/ijse/dep8/orm/package-info.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * A simple ORM framework that creates a database and its tables given the tables by means of classes with <code>@Entity</code> annotation and primary key by means of <code>@Id</code> annotation
+ *
+ * A valid JDBC connection is to be given in order for the framework to connect with JDBC
+ *
+ *
+ * @author Hashadhi-Jayasinghe
+ * @since 1.0.0
+ */
+
+package lk.ijse.dep8.orm;
